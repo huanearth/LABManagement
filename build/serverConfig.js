a646e2552cedd50259598b0c46c62e00f499e5f3
huanearth <42688853+huanearth@users.noreply.github.com>
2020-09-07 22:19:56 +0800

    Add files via upload

---
+++ build/serverConfig.js
@@ -0,0 +1,19 @@
+/**
+* author:苏晓
+* 功能：系统的配置文件
+* 创建时间：2019-04-16
+*/
+let bulidConfig={
+
+    //分页查询默认分页标准
+    defaultPagination : {
+        current: 1,
+        defaultCurrent: 1,
+        pageSize: 10,
+    },
+
+    functionsList:{fund:''},
+
+    serverAddress:'172.27.50.5:8557'
+
+};
